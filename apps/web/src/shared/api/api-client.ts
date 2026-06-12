a88c187943fefdbce3ee6f35c424ac1b0ac3c91b
Hozmel Roberto De la rosa Rendón <hozdelarosar@gmail.com>
2026-06-11 19:22:05 -0600

    fix: mejorar manejo de errores en apiClient
    - Se ha actualizado la función `apiClient` para establecer el encabezado `Content-Type` solo si el cuerpo de la solicitud no es una instancia de `FormData`.
    - Se ha modificado el manejo de errores para lanzar un objeto que incluye el estado de la respuesta y el mensaje de error, mejorando la claridad en la gestión de errores inesperados.

--- apps/web/src/shared/api/api-client.ts
+++ apps/web/src/shared/api/api-client.ts
@@ -10,7 +10,9 @@ export async function apiClient<TResponse>(
   const token = useAuthStore.getState().accessToken
   const headers = new Headers(options.headers)
 
-  headers.set('Content-Type', 'application/json')
+  if (!(options.body instanceof FormData)) {
+    headers.set('Content-Type', 'application/json')
+  }
 
   if (options.auth !== false && token) {
     headers.set('Authorization', `Bearer ${token}`)
@@ -22,52 +24,21 @@ export async function apiClient<TResponse>(
   })
 
   if (!response.ok) {
-    const error = await response.json().catch(() => null)
+    const error = await response.json().catch(() => ({
+      message: 'Error inesperado',
+    }))
 
     if (response.status === 401 && options.auth !== false) {
       useAuthStore.getState().clearSession()
     }
 
-    throw error ?? new Error('Request failed')
+    throw {
+      status: response.status,
+      ...error,
+    }
   }
 
   if (response.status === 204) return undefined as TResponse
 
   return response.json() as Promise<TResponse>
 }
-// import { useAuthStore } from '@/modules/auth/store/auth.store'
-// import type { RequestOptions } from '../types/auth.type'
-
-// const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:3000/api/v1'
-
-// export async function apiClient<TResponse>(
-//   endpoint: string,
-//   options: RequestOptions = {},
-// ): Promise<TResponse> {
-//   const token = useAuthStore.getState().accessToken
-//   const headers = new Headers(options.headers)
-
-//   headers.set('Content-Type', 'application/json')
-
-//   if (options.auth !== false && token)
-//     headers.set('Authorization', `Bearer ${token}`)
-
-//   const response = await fetch(`${API_URL}${endpoint}`, {
-//     ...options,
-//     headers,
-//   })
-
-//   if (response.status === 401) {
-//     useAuthStore.getState().clearSession()
-//     window.location.href = '/login'
-//   }
-
-//   if (!response.ok) {
-//     const error = await response.json().catch(() => null)
-//     throw error ?? new Error('Request failed')
-//   }
-
-//   if (response.status === 204) return undefined as TResponse
-
-//   return response.json() as Promise<TResponse>
-// }
